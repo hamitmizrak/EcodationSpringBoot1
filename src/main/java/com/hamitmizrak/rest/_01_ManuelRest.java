75195aa7ce74c7f8384407b6f1d8aad4ab944687
hamitmizrak <hamitmizrak@gmail.com>
2022-05-14 10:28:15 +0300

    @RestController @PathVariable

--- src/main/java/com/hamitmizrak/rest/_01_ManuelRest.java
+++ src/main/java/com/hamitmizrak/rest/_01_ManuelRest.java
@@ -1,11 +1,14 @@
 package com.hamitmizrak.rest;
 
 import com.hamitmizrak.dto.ProductDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class _01_ManuelRest {
     //https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
 
@@ -47,6 +50,24 @@ public class _01_ManuelRest {
         return productDto;
     }
 
+
+    //http://localhost:8080/rest4/2
+    //http://localhost:8080/rest4
+    @GetMapping( {"rest4","rest4/{id}"}  )
+    public ProductDto getRest4(@PathVariable(name = "id",required = false) Long id){
+        ProductDto productDto=null;
+        if(id==null){
+            log.error("id Null geldi");
+        }else{
+            productDto =ProductDto
+                    .builder()
+                    .productId(id)
+                    .productName("Ürün Adı44")
+                    .productTrade("ürün markası44")
+                    .build();
+        }
+        return productDto;
+    }
 
 
 
