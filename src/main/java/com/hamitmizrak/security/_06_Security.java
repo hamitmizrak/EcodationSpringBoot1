6864be5251f3eed9e59bd6d443a7f7e765eb5c58
hamitmizrak <hamitmizrak@gmail.com>
2022-05-15 11:20:48 +0300

    WebSecurity

--- src/main/java/com/hamitmizrak/security/_06_Security.java
+++ src/main/java/com/hamitmizrak/security/_06_Security.java
@@ -3,27 +3,39 @@ package com.hamitmizrak.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class _05_Security {
-    @Autowired
-    PasswordEncoder passwordEncoder;
+
+public class _06_Security{
+
 }
+
 //@EnableWebSecurity
-//public class _05_Security extends WebSecurityConfigurerAdapter {
+//public class _06_Security extends WebSecurityConfigurerAdapter {
+//   // @Autowired
+//    //PasswordEncoder passwordEncoder;
 //
-//    @Autowired
-//    PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//       http
+//               .authorizeRequests()
+//               .antMatchers("/","/index","/success","/login")
+//               .permitAll()
+//               .anyRequest()
+//               .authenticated();
+//    }
 //
 //
 //    @Autowired
 //    public void ramazanAddUser(AuthenticationManagerBuilder managerBuilder) throws Exception{
 //        managerBuilder
 //                .inMemoryAuthentication() //cache bellek data olsun
-//                .withUser("root")
-//                .password(passwordEncoder.encode("root"))
+//                .withUser("ramazan")
+//                .password("{noop}root")
 //                .roles("ADMIN");
 //    }
 //}
