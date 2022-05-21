06f655bb92f0dc47dd19694bcd6b0831fee8b1a3
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 10:40:59 +0300

    UML

---
+++ src/main/java/com/hamitmizrak/uml/UmlDiagram.java
@@ -0,0 +1,28 @@
+package com.hamitmizrak.uml;
+
+public class UmlDiagram {
+
+    // +
+    public String name;
+    //UML: +name:string
+
+    // -
+    private String surname;
+    //UML: -surname:string
+
+    // #
+    protected long hesCode;
+    //UML: #hesCode:long
+
+    // static=altçizgi
+    public static String verifiy;
+    //UML:  +____verify:string
+
+
+    //Method
+    public void deneme(int sayi,String adi){
+
+    }
+
+
+}
